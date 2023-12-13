5a0c45a4e93e58bed8279b59015f52b5633133e1
OhioLight <142236532+OhioLight@users.noreply.github.com>
2023-12-13 13:03:12 +0100

    bustifulized

--- lib/aufgabe_1.dart
+++ lib/aufgabe_1.dart
@@ -25,50 +25,81 @@ class MainApp extends StatelessWidget {
             ],
           ),
         ),
-        body: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.blue.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.yellow,
+                    blurRadius: 20,
+                  )
+                ]),
+            width: 200,
+            height: 200,
+            child: const Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  'A',
-                  style: TextStyle(
-                    fontSize: 24,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'A',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'B',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'C',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.purple,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 60,
+                    ),
+                    Text(
+                      'D',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
                 ),
                 Text(
-                  'B',
+                  'E',
                   style: TextStyle(
                     fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.yellow,
                   ),
                 ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'C',
-                  style: TextStyle(
-                    fontSize: 24,
-                  ),
-                ),
-                Text(
-                  'D',
-                  style: TextStyle(
-                    fontSize: 24,
-                  ),
-                ),
-              ],
-            ),
-            Text(
-              'E',
-              style: TextStyle(
-                fontSize: 24,
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
